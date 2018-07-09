2d0e51f026e446851777795c0ddd7d4fb9b44a2d
Mateusz Urbański <matek2305@gmail.com>
2018-07-09 22:35:56 +0200

    test for GET /points endpoint

--- src/test/scala/com/github/matek2305/djamoe/CompetitionRestServiceSpec.scala
+++ src/test/scala/com/github/matek2305/djamoe/CompetitionRestServiceSpec.scala
@@ -6,7 +6,7 @@ import akka.actor.ActorRef
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.testkit.{TestActor, TestProbe}
-import com.github.matek2305.djamoe.CompetitionAggregate.GetAllMatches
+import com.github.matek2305.djamoe.CompetitionAggregate.{GetAllMatches, GetPoints}
 import org.scalatest.{Matchers, WordSpec}
 import spray.json._
 
@@ -14,11 +14,12 @@ class CompetitionRestServiceSpec extends WordSpec with Matchers with ScalatestRo
 
   "Competition rest service" should {
 
-    "return list of all matches for GET requests to the /matches path" in {
-      val probe = TestProbe()
-      val service = new CompetitionRestService(probe.ref)
+    val probe = TestProbe()
+    val service = new CompetitionRestService(probe.ref)
 
-      probe.setAutoPilot((sender: ActorRef, msg: Any) => {
+    "return list of all matches for GET requests to the /matches path" in {
+
+      probe.setAutoPilot((sender: ActorRef, _: Any) => {
         sender ! List(
           MatchState(Match(
             "France",
@@ -41,7 +42,29 @@ class CompetitionRestServiceSpec extends WordSpec with Matchers with ScalatestRo
         ))
       }
     }
+
+    "return players points for GET requests to the /points path" in {
+
+      probe.setAutoPilot((sender: ActorRef, _: Any) => {
+        sender ! Map(
+          "Foo" -> 5,
+          "Bar" -> 2,
+          "Baz" -> 0
+        )
+        TestActor.KeepRunning
+      })
+
+      Get("/points") ~> service.route ~> check {
+        probe.expectMsg(GetPoints())
+
+        status shouldEqual StatusCodes.OK
+
+        responseAs[String].parseJson shouldEqual JsObject(
+          "Foo" -> JsNumber(5),
+          "Bar" -> JsNumber(2),
+          "Baz" -> JsNumber(0)
+        )
+      }
+    }
   }
-
-
 }
