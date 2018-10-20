837220ad0c54e7ac3b1b0ec65427c34395e4a2c0
Mateusz Urbański <matek2305@gmail.com>
2018-10-20 09:20:16 +0200

    avro serializer wip

--- src/main/scala/com/github/matek2305/djamoe/app/AvroSerializer.scala
+++ src/main/scala/com/github/matek2305/djamoe/app/AvroSerializer.scala
@@ -1,11 +1,13 @@
 package com.github.matek2305.djamoe.app
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.util.UUID
 
 import akka.serialization.SerializerWithStringManifest
-import com.github.matek2305.djamoe.domain.CompetitionEvent.MatchFinished
-import com.github.matek2305.djamoe.domain.Score
-import com.sksamuel.avro4s.{AvroInputStream, AvroOutputStream}
+import com.github.matek2305.djamoe.domain.CompetitionEvent.{BetMade, BettingLocked, MatchAdded, MatchFinished}
+import com.github.matek2305.djamoe.domain.MatchId
+import com.sksamuel.avro4s._
+import org.apache.avro.Schema
 
 class AvroSerializer extends SerializerWithStringManifest {
 
@@ -13,23 +15,56 @@ class AvroSerializer extends SerializerWithStringManifest {
 
   override def manifest(o: AnyRef): String = o.getClass.getName
 
-  final val MatchFinishedManifest = classOf[Score].getName
+  final val MatchAddedManifest = classOf[MatchAdded].getName
+  final val MatchFinishedManifest = classOf[MatchFinished].getName
+  final val BetMadeManifest = classOf[BetMade].getName
+  final val BettingLockedManifest = classOf[BettingLocked].getName
+
+  implicit object MatchIdFromValue extends FromValue[MatchId] {
+    override def apply(value: Any, field: Schema.Field): MatchId = {
+      MatchId(UUID.fromString(value.toString))
+    }
+  }
 
   override def toBinary(o: AnyRef): Array[Byte] = o match {
-    case finished: Score =>
-      val baos = new ByteArrayOutputStream()
-      val output = AvroOutputStream.binary[Score](baos)
-      output.write(finished)
-      output.close()
-      baos.toByteArray
+    case added: MatchAdded => toBinaryByClass(added)
+    case finished: MatchFinished => toBinaryByClass(finished)
+    case made: BetMade => toBinaryByClass(made)
+    case locked: BettingLocked => toBinaryByClass(locked)
     case _ => throw new IllegalArgumentException(s"Unknown event to serialize: $o")
+  }
+
+  def toBinaryByClass[T: SchemaFor : ToRecord](event: T): Array[Byte] = {
+    val os = new ByteArrayOutputStream()
+    val output = AvroOutputStream.binary[T](os)
+
+    output.write(event)
+    output.flush()
+    output.close()
+
+    os.toByteArray
   }
 
   override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = {
     manifest match {
+      case MatchAddedManifest =>
+        val is = new ByteArrayInputStream(bytes)
+        val input = AvroInputStream.binary[MatchAdded](is)
+        is.close()
+        input.iterator.toSeq.head
       case MatchFinishedManifest =>
         val is = new ByteArrayInputStream(bytes)
-        val input = AvroInputStream.binary[Score](is)
+        val input = AvroInputStream.binary[MatchFinished](is)
+        is.close()
+        input.iterator.toSeq.head
+      case BetMadeManifest =>
+        val is = new ByteArrayInputStream(bytes)
+        val input = AvroInputStream.binary[BetMade](is)
+        is.close()
+        input.iterator.toSeq.head
+      case BettingLockedManifest =>
+        val is = new ByteArrayInputStream(bytes)
+        val input = AvroInputStream.binary[BettingLocked](is)
         is.close()
         input.iterator.toSeq.head
       case _ => throw new IllegalArgumentException(s"Unable to handle manifest: $manifest")
